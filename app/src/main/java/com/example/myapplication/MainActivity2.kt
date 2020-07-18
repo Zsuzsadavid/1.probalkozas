3abb2312d54db224429d1d17a5b04e75bbe5e797
Zsuzsadavid <zsuzsadavid44@gmail.com>
2020-07-18 13:53:15 +0200

    Csak sikerulni fog

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`activity_main2_A_R_G_O_S bemutatkozik`)
+    }
+}
